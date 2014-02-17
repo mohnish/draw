43a3cc170996bc9a067726092ff46f5e2e09b3af
Mohnish Thallavajhula <i@mohni.sh>
2014-02-16 17:26:55 -0800

    Add pub sub to painter

--- painter.js
+++ painter.js
@@ -1,45 +1,51 @@
 
 ;(function(window) {
-  function Painter(el) {
+  function Painter(el, node) {
     if (!(this instanceof Painter)) return new Painter(el);
     if (!el) throw new Error('A HTML Canvas element is required to operate on');
     this.el = el;
     this.ctx = el.getContext('2d');
     this.inProgress = false;
     this.eventManager = EventManager(this.el, this);
+    this.node = node;
     this.attachEvents();
+    this.subscribe();
   }
 
   Painter.prototype.attachEvents = function() {
     this.eventManager.bind('touchstart', this.start);
-    this.eventManager.bind('touchmove', this.paint);
-    this.eventManager.bind('touchend', this.stop);
     this.eventManager.bind('mousedown', this.start);
+    this.eventManager.bind('touchmove', this.paint);
     this.eventManager.bind('mousemove', this.paint);
+    this.eventManager.bind('touchend', this.stop);
     this.eventManager.bind('mouseup', this.stop);
     return this;
   };
 
-  Painter.prototype.start = function(e) {
-    e.preventDefault();
-    var x = e.x || e.touches[0].pageX
-      , y = e.y || e.touches[0].pageY;
+  Painter.prototype.start = function(e, x, y) {
+    var x = x || e.x || e.touches[0].pageX
+      , y = y || e.y || e.touches[0].pageY;
+
+    e && e.preventDefault();
 
     this.ctx.strokeStyle = "rgb(200,0,0)";
     this.inProgress = true;
     this.ctx.beginPath();
     this.ctx.moveTo(x, y);
+    this.publish('start', x, y);
     return this;
   };
 
-  Painter.prototype.paint = function(e) {
-    e.preventDefault();
-    var x = e.x || e.touches[0].pageX
-      , y = e.y || e.touches[0].pageY;
+  Painter.prototype.paint = function(e, x, y) {
+    var x = x || e.x || e.touches[0].pageX
+      , y = y || e.y || e.touches[0].pageY;
+
+    e && e.preventDefault();
 
     if (this.inProgress) {
       this.ctx.lineTo(x, y);
       this.ctx.stroke();
+      this.publish('paint', x, y);
     }
 
     return this;
@@ -48,6 +54,7 @@
   Painter.prototype.stop = function() {
     this.ctx.closePath();
     this.inProgress = false;
+    this.publish('stop', null, null);
     return this;
   };
 
@@ -69,6 +76,26 @@
     this.eventManager.unbind('touchmove', this.paint);
     this.eventManager.unbind('touchend', this.stop);
     return this;
+  };
+
+  Painter.prototype.publish = function(action, x, y) {
+    this.node && this.node.set({ action: action, x: x, y: y });
+  };
+
+  Painter.prototype.subscribe = function() {
+    var self = this;
+
+    this.node && this.node.on('value', function(snapshot) {
+      var data = snapshot.val();
+
+      if ('start' == data.action) {
+        self.start(null, data.x, data.y);
+      } else if ('paint' == data.action) {
+        self.paint(null, data.x, data.y);
+      } else {
+        self.stop();
+      }
+    });
   };
 
   window.Painter = Painter;
