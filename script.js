e4ccd38fae4fc12869ce64305505751adab5328d
Mohnish Thallavajhula <i@mohni.sh>
2014-02-16 17:27:31 -0800

    update constructor

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 
-var painter = new Painter(document.querySelector('#draw-zone'));
+var dataRef = new Firebase('https://testdraw.firebaseio.com/')
+  , node = dataRef.child('points')
+  , painter = new Painter(document.querySelector('#draw-zone'), node);
